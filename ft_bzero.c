971497001049778c8da54422cfb1cc6b5325be8a
RafaelMS <martinsoeder35@gmail.com>
2025-04-08 20:20:56 +0200

    pasan norminette, asegurar atoi, strlcpy, is, añadido bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,8 +6,42 @@
 /*   By: rafael-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 19:38:06 by rafael-m          #+#    #+#             */
-/*   Updated: 2025/04/08 19:38:35 by rafael-m         ###   ########.fr       */
+/*   Updated: 2025/04/08 20:20:30 by rafael-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
 
+void	*ft_bzero(void *s, size_t n)
+{
+	size_t			i;
+	unsigned char	*d;
+
+	i = 0;
+	d = (unsigned char *)s;
+	while (i < n)
+	{
+		d[i] = '\0';
+		i++;
+	}
+	return (s);
+}
+/*
+int	main(void)
+{
+	size_t	n = 10;
+	char	*s;
+	char	*s2;
+
+	s = (char *)malloc(n * sizeof(int));
+	s2 = (char *)malloc(n * sizeof(int));
+	ft_bzero(s, n);
+	printf("s = %s\n", s);
+	bzero(s2, n);
+	printf("s2 = %s\n", s2);
+	free (s);
+}
+*/
