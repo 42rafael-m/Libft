56853fba49a0cb4b05dc02985dd1531e834954bb
RafaelMS <martinsoeder35@gmail.com>
2025-04-10 15:14:13 +0200

    calloc terminado

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,37 +3,60 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+void	*ft_bzero(void *s, size_t n)
+{
+	size_t			i;
+	unsigned char	*d;
+
+	i = 0;
+	d = (unsigned char *)s;
+	while (i < n)
+	{
+		d[i] = '\0';
+		i++;
+	}
+	return (s);
+}
+
+
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	type	*result;
+	void	*result;
 
-/*	if (nmemb * size > PTRDIFF_MAX)
-		return (NULL);
 	if (nmemb == 0 || size == 0)
+	{
+		result = malloc(0);
+		return (result);
+	}
+	if (nmemb > SIZE_MAX / size)
 		return (NULL);
-	result = (char *)malloc(nmemb * size);
-	return (result);*/
-	ressult = (type *)malloc(nmemb, size);
+	result = malloc(nmemb * size);
+	if (!result)
+		return (NULL);
+	ft_bzero(result, nmemb * size);
 	return (result);
 }
 
 int	main(void)
 {
 	size_t	size = 4;
-	size_t	nmemb = 7;
+	size_t	nmemb = 0;
 	size_t	i = 0;
-	char	*r1 = ft_calloc(nmemb, size);
-	char    *r2 = calloc(nmemb, size);
+	char	*r1 = (char *)ft_calloc(nmemb, size);
+	char    *r2 = (char *)calloc(nmemb, size);
 
 	while (i < (nmemb * size))
 	{
 		r1[i] = 65;
 		r2[i] = 65;
 		i++;
-	}	
-	printf("r1 = %ld\n", strlen(r1));
-	printf("r2 = %ld\n", strlen(r2));
-	printf("PTRDIFF_MAX = %ld\n", PTRDIFF_MAX);
+	}
+	printf("r1 = %p\n", r1);
+	printf("r2 = %p\n", r2);
+	printf("len r1 = %ld\n", strlen(r2));
+	printf("len r2 = %ld\n", strlen(r2));
+	printf("r1 = %s\n", r1);
+	printf("r2 = %s\n", r2);
 	free (r1);
 	free (r2);
 }
